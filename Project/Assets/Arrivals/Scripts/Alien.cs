3ff2494f8a9c596b8c39553c16e46bb71d4155fb
suibiweng <suibi.weng@gmail.com>
2019-01-18 19:05:56 -0500

    Starting the project

---
+++ Project/Assets/Arrivals/Scripts/Alien.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class Alien : MonoBehaviour {
+    public Animator AlienAni;
+    public Transform parentOutside;
+    public Transform mainPlayer;
+
+  //  public MLHandKeyPose _HandKeyPose;
+    //public SpriteRenderer[] allHints;
+	// Use this for initialization
+	void Start () {
+        //AlienAni = this.GetComponent<Animator>();
+
+
+        //MLHandKeyPose.Thumb
+
+
+
+    }
+
+
+
+    public void changetoOutside(){
+
+        transform.SetParent(parentOutside);
+
+        transform.LookAt(new Vector3(mainPlayer.position.x,this.transform.position.y,mainPlayer.position.z));
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Alpha1))
+            controlGesture(1);
+        if (Input.GetKey(KeyCode.Alpha2))
+            controlGesture(2);
+        if (Input.GetKey(KeyCode.Alpha3))
+            controlGesture(3);
+
+
+
+
+	}
+
+    /*
+    void setHint(int hindex ,bool onoff){
+        if(onoff){
+            allHints[hindex].color = new Color(1.0f, 0, 0);
+        }else{
+            allHints[hindex].color = new Color(0, 0, 0);
+        }
+
+
+    }*/
+
+
+
+
+
+    void controlGesture(int s){
+
+        AlienAni.SetInteger("Gesture",s);
+
+
+
+    }
+
+    public void C(){
+
+
+        controlGesture(1);
+    }
+
+
+
+  
+
+
+    public void OK()
+    {
+
+        controlGesture(2);
+
+    }
+
+
+
+
+
+    public void Thumb()
+    {
+
+        controlGesture(3);
+
+    }
+
+
+
+
+
+}
---
+++ Project/Assets/Arrivals/Scripts/KeyGestureControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+public class KeyGestureControl : MonoBehaviour {
+
+    public string Functioncall;
+
+    public  MLHandKeyPose _keyPoseToTrack;
+
+    private bool _trackLeftHand = true;
+    private bool _trackRightHand = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        float confidenceLeft = _trackLeftHand ? GetKeyPoseConfidence(MLHands.Left) : 0.0f;
+        float confidenceRight = _trackRightHand ? GetKeyPoseConfidence(MLHands.Right) : 0.0f;
+        float confidenceValue = Mathf.Max(confidenceLeft, confidenceRight);
+
+     //   Color currentColor = Color.white;
+
+        if (confidenceValue > 0.0f)
+        {
+            BroadcastMessage(Functioncall);
+        }
+
+    
+    
+    
+    
+    }
+
+
+    private float GetKeyPoseConfidence(MLHand hand)
+    {
+        if (hand != null)
+        {
+            if (hand.KeyPose == _keyPoseToTrack)
+            {
+                return hand.KeyPoseConfidence;
+            }
+        }
+        return 0.0f;
+    }
+}
